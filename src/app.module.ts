1299a40e30ece015984bea8aae98ab1a120a98a9
AGelstein <gelstein.andrew@gmail.com>
2024-09-10 17:14:19 -0500

    added controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './controllers/cat/cat.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  controllers: [CatsController],
   providers: [AppService],
 })
 export class AppModule {}
